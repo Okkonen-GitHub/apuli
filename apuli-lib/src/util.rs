1a4f0b09899f58dfc720b4c850d13ba43155933a
Okko <okko.contact@proton.me>
2025-07-15 21:33:25 +0300

    add cache_insert back

---
+++ apuli-lib/src/util.rs
@@ -0,0 +1,16 @@
+use core::hash::Hash;
+use std::collections::HashMap;
+
+pub fn cache_insert<K, V>(cache: &mut HashMap<K, Vec<V>>, k: K, v: V)
+where
+    K: Eq + Hash,
+    V: Clone,
+{
+    if cache.contains_key(&k) {
+        let mut positions = cache.get(&k).unwrap().clone();
+        positions.push(v);
+        cache.insert(k, positions);
+    } else {
+        cache.insert(k, vec![v]);
+    }
+}
