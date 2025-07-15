e7b1fc1dd49bafac7ba785122611955b07f0e48c
Okko <okko.contact@proton.me>
2025-07-15 21:33:27 +0300

    add a simple tui beginning

--- apuli-dbg/src/main.rs
+++ apuli-dbg/src/main.rs
@@ -1,7 +1,38 @@
 use apuli_lib::apuli::query;
+use crossterm::{
+    event::{self, KeyCode, KeyEventKind},
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
+use ratatui::{
+    prelude::{CrosstermBackend, Stylize, Terminal},
+    widgets::Paragraph,
+};
+use std::io::{stdout, Result};
 
-fn main() {
+fn main() -> Result<()> {
     let result = query(&[], None, None, 5);
     let first = result.first().unwrap();
-    println!("{first}");
+
+    stdout().execute(EnterAlternateScreen)?;
+    enable_raw_mode()?;
+    let mut term = Terminal::new(CrosstermBackend::new(stdout()))?;
+
+    loop {
+        term.draw(|frame| {
+            let area = frame.size();
+            frame.render_widget(Paragraph::new(format!("{first}")).white().on_black(), area);
+        })?;
+        if event::poll(std::time::Duration::from_millis(16))? {
+            if let event::Event::Key(key) = event::read()? {
+                if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
+                    break;
+                }
+            }
+        }
+    }
+
+    stdout().execute(LeaveAlternateScreen)?;
+    disable_raw_mode()?;
+    Ok(())
 }
